a37b31547c0861bbb97021e345827157f4af53f3
Goran Ribic <goran.ribic@frontify.com>
2022-02-09 12:08:39 +0100

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div>
-      <header>DEMO APP TEST</header>
-    </div>
-  );
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  ORDER_BOOK_PAGE,
+  DEFAULT_ORDER_BOOK_PAGE,
+  ERROR_PAGE,
+} from "./constants";
+import { ErrorPage, OrderBookPage } from "./pages";
 
-export default App;
+export const App = (): React.ReactElement => {
+  return (
+    <Routes>
+      <Route path={ORDER_BOOK_PAGE} element={<OrderBookPage />} />
+      <Route path={ERROR_PAGE} element={<ErrorPage />} />
+      <Route
+        path="/"
+        element={<Navigate to={DEFAULT_ORDER_BOOK_PAGE} replace />}
+      />
+      <Route path="*" element={<Navigate to={ERROR_PAGE} replace />} />
+    </Routes>
+  );
+};
