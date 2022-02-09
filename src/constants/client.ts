2082a4e9d588cb50da3ca86b843738b65a1f1293
Goran Ribic <goran.ribic@frontify.com>
2022-02-09 12:06:22 +0100

    add: root constants

---
+++ src/constants/client.ts
@@ -0,0 +1,12 @@
+export const ORDER_BOOK_PAGE = "orderbook/:pair";
+export const DEFAULT_ORDER_BOOK_PAGE = "orderbook/BTC_USDT";
+export const ERROR_PAGE = "error";
+export const EMPTY_STRING = "";
+export const ERROR_MSG_INVALID_DATA =
+  "Data was not sent properly, please check input data and try again";
+export const ERROR_MSG_NOT_AUTHORIZED =
+  "You are not authorized to see this content";
+export const ERROR_MSG_NOT_FOUND = "Resource not found";
+export const ERROR_MSG_500 =
+  "Ooops! Something went wrong, please contact support";
+export const ERROR_MSG_SERVICE = "Service is not available";
